57f5baf9eb8e79876f35585a12849a15f35590a2
Jon Tyler <k98kurz@gmail.com>
2023-06-02 09:53:07 -0400

    OP_FALSE through OP_SIZE tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,10 +1,22 @@
 from context import classes, functions
 from hashlib import sha256, sha3_256
+from queue import LifoQueue
 from random import randint
 import unittest
 
 
 class TestFunctions(unittest.TestCase):
+    tape: classes.Tape
+    queue: LifoQueue
+    cache: dict
+
+    def setUp(self) -> None:
+        self.tape = classes.Tape(b'')
+        self.queue = LifoQueue()
+        self.cache = {}
+        return super().setUp()
+
+    # helper functions
     def test_bytes_to_int_raises_errors_for_invalid_input(self):
         with self.assertRaises(TypeError) as e:
             functions.bytes_to_int('not bytes')
@@ -48,6 +60,99 @@ class TestFunctions(unittest.TestCase):
     def test_bytes_to_bool_returns_bool(self):
         assert functions.bytes_to_bool(b'') is False
         assert functions.bytes_to_bool(b'1') is True
+
+    # ops
+    def test_OP_FALSE_puts_null_byte_onto_queue(self):
+        assert self.queue.empty()
+        assert not len(self.cache.keys())
+        functions.OP_FALSE(self.tape, self.queue, self.cache)
+        assert not self.queue.empty()
+        assert not len(self.cache.keys())
+        assert self.queue.get() == b'\x00'
+
+    def test_OP_TRUE_puts_nonnull_byte_onto_queue(self):
+        assert self.queue.empty()
+        assert not len(self.cache.keys())
+        functions.OP_TRUE(self.tape, self.queue, self.cache)
+        assert not self.queue.empty()
+        assert not len(self.cache.keys())
+        assert self.queue.get() == b'\x01'
+
+    def test_OP_PUSH0_puts_next_byte_from_tape_onto_queue(self):
+        self.tape = classes.Tape(b'123')
+        assert self.queue.empty()
+        assert not len(self.cache.keys())
+        functions.OP_PUSH0(self.tape, self.queue, self.cache)
+        assert not self.queue.empty()
+        assert not len(self.cache.keys())
+        assert self.queue.get() == b'1'
+
+    def test_OP_PUSH1_reads_next_byte_as_int_and_puts_that_many_from_tape_onto_queue(self):
+        self.tape = classes.Tape(functions.int_to_bytes(11) + b'hello world')
+        assert self.queue.empty()
+        assert not len(self.cache.keys())
+        functions.OP_PUSH1(self.tape, self.queue, self.cache)
+        assert not self.queue.empty()
+        assert not len(self.cache.keys())
+        assert self.queue.get() == b'hello world'
+
+    def test_OP_PUSH2_reads_next_2_bytes_as_int_and_puts_that_many_from_tape_onto_queue(self):
+        self.tape = classes.Tape(b'\x00' + functions.int_to_bytes(11) + b'hello world')
+        assert self.queue.empty()
+        assert not len(self.cache.keys())
+        functions.OP_PUSH2(self.tape, self.queue, self.cache)
+        assert not self.queue.empty()
+        assert not len(self.cache.keys())
+        assert self.queue.get() == b'hello world'
+
+    def test_OP_PUSH4_reads_next_4_bytes_as_int_and_puts_that_many_from_tape_onto_queue(self):
+        self.tape = classes.Tape(b'\x00\x00\x00' + functions.int_to_bytes(11) + b'hello world')
+        assert self.queue.empty()
+        assert not len(self.cache.keys())
+        functions.OP_PUSH4(self.tape, self.queue, self.cache)
+        assert not self.queue.empty()
+        assert not len(self.cache.keys())
+        assert self.queue.get() == b'hello world'
+
+    def test_OP_POP0_moves_one_item_from_queue_into_cache(self):
+        self.queue.put(b'1234')
+        assert not self.queue.empty()
+        assert not len(self.cache.keys())
+        functions.OP_POP0(self.tape, self.queue, self.cache)
+        assert self.queue.empty()
+        assert len(self.cache.keys())
+        assert b'P' in self.cache
+        assert self.cache[b'P'] == [b'1234']
+
+    def test_OP_POP1_reads_next_bytes_from_tape_then_puts_that_many_items_from_queue_to_cache(self):
+        assert self.queue.empty()
+        self.queue.put(b'12')
+        self.queue.put(b'34')
+        self.tape = classes.Tape(functions.int_to_bytes(2))
+        assert not self.queue.empty()
+        assert b'P' not in self.cache
+        functions.OP_POP1(self.tape, self.queue, self.cache)
+        assert self.queue.empty()
+        assert b'P' in self.cache
+        assert self.cache[b'P'] == [b'34', b'12']
+
+    def test_OP_POP1_interprets_negative_ints_as_positive(self):
+        assert self.queue.empty()
+        for i in range(136):
+            self.queue.put(functions.int_to_bytes(i))
+        self.tape = classes.Tape(functions.int_to_bytes(-120))
+        functions.OP_POP1(self.tape, self.queue, self.cache)
+        assert self.queue.empty()
+        assert b'P' in self.cache
+        assert len(self.cache[b'P']) == 136
+
+    def test_OP_SIZE_pulls_item_from_queue_and_puts_its_length_onto_queue(self):
+        assert self.queue.empty()
+        self.queue.put(b'123')
+        functions.OP_SIZE(self.tape, self.queue, self.cache)
+        assert not self.queue.empty()
+        item = self.queue.get(False)
+        assert item == functions.int_to_bytes(3)
 
 
 if __name__ == '__main__':
