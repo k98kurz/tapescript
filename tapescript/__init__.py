6f0368581763d42772053b48ed7fc97bd1ca8dc5
Jon Tyler <k98kurz@gmail.com>
2023-06-05 21:56:39 -0400

    added run_auth_script as a primary module function

--- tapescript/__init__.py
+++ tapescript/__init__.py
@@ -2,7 +2,8 @@ from .classes import Tape
 from .errors import ScriptExecutionError
 from .functions import (
     run_script,
-    run_tape
+    run_tape,
+    run_auth_script
 )
 from .parsing import (
     compile_script,
